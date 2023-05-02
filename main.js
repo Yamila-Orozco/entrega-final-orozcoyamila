5dd5995b86542360fe1134a1ccbccc89cbd2e646
Yamila Orozco <orozco.yamila.gisele@gmail.com>
2023-05-02 19:04:49 -0300

    more changes

---
+++ main.js
@@ -0,0 +1,19 @@
+/*tipos de datos*/
+545454545 //numbers
+"Esto es un string" // string de characters
+true // es un valor booleano
+false // es un valor booleano
+
+null // nulo
+
+undefined //tipo de dato que no esta definido
+
+/*variables* espacio reservado de la memoria que me permite almacenar datos utiles para la ejecicion de mi programa simulador*/
+//declaro una variable
+let nombre;
+var apellido; //no se utiliza mas
+
+nombre = "samuel";
+
+//incializar con un dato
+let edad = 18;
